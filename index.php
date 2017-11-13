1304f0cef0a7edf59e45a4283d3e40e81af1ad91
Makarov Sergii <oreymgt@gmail.com>
2017-11-13 15:10:28 +0200

    Create a full page - Settings

---
+++ index.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta property="og:image" content="<?= $siteSettings->generalUrl; ?>path/to/image.jpg">
+    <!-- Chrome, Firefox OS and Opera -->
+    <meta name="theme-color" content="#000">
+    <!-- Windows Phone -->
+    <meta name="msapplication-navbutton-color" content="#000">
+    <!-- iOS Safari -->
+    <meta name="apple-mobile-web-app-status-bar-style" content="#000">
+
+    <title><?= $siteSettings->siteTitle ?></title>
+
+    <link rel="shortcut icon" href="<?= $siteSettings->generalUrl; ?>img/favicon/favicon.ico" type="image/x-icon">
+    <link rel="apple-touch-icon" href="<?= $siteSettings->generalUrl; ?>img/favicon/apple-touch-icon.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="<?= $siteSettings->generalUrl; ?>img/favicon/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="<?= $siteSettings->generalUrl; ?>img/favicon/apple-touch-icon-114x114.png">
+    <!-- Lib CSS -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700">
+    <link rel="stylesheet" type="text/css" href="<?= $siteSettings->generalUrl; ?>libs/materialize/css/materialize.min.css">
+    <link rel="stylesheet" type="text/css" href="<?= $siteSettings->generalUrl; ?>libs/OwlCarousel/dist/assets/owl.carousel.min.css">
+    <!-- My CSS -->
+    <link rel="stylesheet" type="text/css" href="<?= $siteSettings->generalUrl; ?>libs/my-fonts/css/icons-style.css">
+    <link rel="stylesheet" type="text/css" href="<?= $siteSettings->generalUrl; ?>css/main.min.css">
+</head>
+<body>
+
+
+
+<!--Scripts-->
+<script type="text/javascript" src="<?= $siteSettings->generalUrl; ?>libs/jquery/dist/jquery.min.js"></script>
+<script type="text/javascript" src="<?= $siteSettings->generalUrl; ?>libs/materialize/js/materialize.min.js"></script>
+<script src="<?= $siteSettings->generalUrl; ?>libs/OwlCarousel/dist/owl.carousel.min.js"></script>
+<script src="<?= $siteSettings->generalUrl; ?>js/common.js"></script>
+
+</body>
+</html>
