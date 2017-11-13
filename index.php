3fe2a768ccf047755c013e3eefd03d46c84650c9
Makarov Sergii <oreymgt@gmail.com>
2017-11-13 16:30:16 +0200

    Correct mistake

--- index.php
+++ index.php
@@ -237,6 +237,31 @@
     </nav>
 </header>
 
+
+<!-- Login Modal Window-->
+<div id="login-modal" class="modal">
+    <div class="modal-content">
+        <div class="container">
+            <div class="row">
+                <div class="col s12 m12 l12 login-btn">
+                    <a class="btn btn-large" href="https://www.facebook.com/v2.9/dialog/oauth?client_id=<?=$siteSettings->socialAcc->fbId?>&redirect_uri=<?=$siteSettings->socialAcc->fbUrl?>&response_type=code&scope=public_profile,email,user_location">
+                        <span>Sign In</span> 
+                        Facebook
+                    </a>
+                </div>
+                <div class="col m12 login-btn">
+                    <a class="btn btn-large" href="https://oauth.vk.com/authorize?client_id=<?=$siteSettings->socialAcc->vcId?>&display=page&redirect_uri=<?=$siteSettings->socialAcc->vcUrl?>&response_type=code">
+                        <span>Sign In</span> 
+                        VContacte
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<!-- Top section -->
 <section id="top-section">
     <div class="container">
         <div class="row">
@@ -278,28 +303,6 @@
         </div>
     </div>
 </section>
-
-<!-- Login Modal Window-->
-<div id="login-modal" class="modal">
-    <div class="modal-content">
-        <div class="container">
-            <div class="row">
-                <div class="col s12 m12 l12 login-btn">
-                    <a class="btn btn-large" href="https://www.facebook.com/v2.9/dialog/oauth?client_id=<?=$siteSettings->socialAcc->fbId?>&redirect_uri=<?=$siteSettings->socialAcc->fbUrl?>&response_type=code&scope=public_profile,email,user_location">
-                        <span>Sign In</span> 
-                        Facebook
-                    </a>
-                </div>
-                <div class="col m12 login-btn">
-                    <a class="btn btn-large" href="https://oauth.vk.com/authorize?client_id=<?=$siteSettings->socialAcc->vcId?>&display=page&redirect_uri=<?=$siteSettings->socialAcc->vcUrl?>&response_type=code">
-                        <span>Sign In</span> 
-                        VContacte
-                    </a>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
 
 <!--Scripts-->
 <script type="text/javascript" src="<?= $siteSettings->generalUrl; ?>libs/jquery/dist/jquery.min.js"></script>
