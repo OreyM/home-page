cffd91432033a70c643735fe81dfe4dfa0326994
Makarov Sergii <oreymgt@gmail.com>
2017-11-13 15:04:50 +0200

    First settings for home page

---
+++ scripts_view.php
@@ -0,0 +1,8 @@
+<!--Scripts-->
+<script type="text/javascript" src="<?= $siteSettings->generalUrl; ?>libs/jquery/dist/jquery.min.js"></script>
+<script type="text/javascript" src="<?= $siteSettings->generalUrl; ?>libs/materialize/js/materialize.min.js"></script>
+<script src="<?= $siteSettings->generalUrl; ?>libs/OwlCarousel/dist/owl.carousel.min.js"></script>
+<script src="<?= $siteSettings->generalUrl; ?>js/common.js"></script>
+
+</body>
+</html>
