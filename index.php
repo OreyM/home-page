b61f4e7160d108568361e8a1caf1f239905d3cbf
Makarov Sergii <oreymgt@gmail.com>
2017-11-13 16:24:16 +0200

    Add top section to full page

--- index.php
+++ index.php
@@ -237,6 +237,48 @@
     </nav>
 </header>
 
+<section id="top-section">
+    <div class="container">
+        <div class="row">
+            <div class="col m6 top-section-left hide-on-med-and-down">
+                <div class="container">
+                    <div class="row">
+                        <div class="col m12">
+                            <i class="small material-icons left">search</i>
+                            <div class="dashed-border">
+                                <a href="#!" title="<?php $homePage->topSection->title1 ?>">
+                                    <span><?php $homePage->topSection->title1 ?></span>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col m12">
+                            <i class="small material-icons left">thumb_up</i>
+                            <div class="dashed-border">
+                                <a href="#!" title="<?php $homePage->topSection->title2 ?>">
+                                    <span><?php $homePage->topSection->title2 ?></span>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col m12">
+                            <i class="small material-icons left">attach_money</i>
+                            <div class="dashed-border">
+                                <a href="#!" title="<?php $homePage->topSection->title3 ?>">
+                                    <span><?php $homePage->topSection->title3 ?></span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col m6 s12 top-section-right">
+                <div class="top-title">
+                    <h1><?php $homePage->topSection->tagline ?></h1>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
 <!-- Login Modal Window-->
 <div id="login-modal" class="modal">
     <div class="modal-content">
