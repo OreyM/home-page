e95d677e51f4db601781564ee33f8a680ae9f07b
Makarov Sergii <oreymgt@gmail.com>
2017-11-13 16:23:25 +0200

    Create top section

---
+++ top_section_view.php
@@ -0,0 +1,41 @@
+<section id="top-section">
+    <div class="container">
+        <div class="row">
+            <div class="col m6 top-section-left hide-on-med-and-down">
+                <div class="container">
+                    <div class="row">
+                        <div class="col m12">
+                            <i class="small material-icons left">search</i>
+                            <div class="dashed-border">
+                                <a href="#!" title="<?php $homePage->topSection->title1 ?>">
+                                    <span><?php $homePage->topSection->title1 ?></span>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col m12">
+                            <i class="small material-icons left">thumb_up</i>
+                            <div class="dashed-border">
+                                <a href="#!" title="<?php $homePage->topSection->title2 ?>">
+                                    <span><?php $homePage->topSection->title2 ?></span>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col m12">
+                            <i class="small material-icons left">attach_money</i>
+                            <div class="dashed-border">
+                                <a href="#!" title="<?php $homePage->topSection->title3 ?>">
+                                    <span><?php $homePage->topSection->title3 ?></span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col m6 s12 top-section-right">
+                <div class="top-title">
+                    <h1><?php $homePage->topSection->tagline ?></h1>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
